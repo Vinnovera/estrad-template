a90c784cd84220335ba065af690f87fa418da6f8
Per Stenström <per.stenstrom@gmail.com>
2014-04-17 16:49:52 +0200

    Undefined partial

--- test/testET.js
+++ test/testET.js
@@ -8,7 +8,8 @@ describe('Estrad Template', function(){
 		page = new Buffer("<div>foo</div>{{=part.test}}{{=part.test}}"),
 		nestedPage = new Buffer("<div>foo</div>{{=part.sndlvl}}"),
 		variantPage = new Buffer("<div>foo</div>{{=part.test.sndlvl}}"),
-		undefPage = new Buffer("<div>foo</div>{{=part.test.sndlvl}}{{=part.test.undef}}")
+		undefObjPage = new Buffer("<div>foo</div>{{=part.test.undef}}"),
+		undefPage = new Buffer("<div>foo</div>{{=part.undef}}"),
 		obj  = {
 			folder: 'test/modules'
 		};
@@ -43,11 +44,21 @@ describe('Estrad Template', function(){
 		});
 	});
 
-	describe('Undefined', function() {
-		it('should interpolate test.json inside test/sndlvl.json and leave {{=part.test.undef}} intact', function(done){
+	describe('Undefined Object', function() {
+		it('should leave the undefined "it" property intact', function(done){
+			partials(undefObjPage, obj, function(err, content){
+				if(err) throw err;
+				assert.equal(content, "<div>foo</div><div>{{!it.bar}}</div>");
+				done();
+			});
+		});
+	});
+
+	describe('Undefined Page', function() {
+		it('should leave the undefined "part" property intact', function(done){
 			partials(undefPage, obj, function(err, content){
 				if(err) throw err;
-				assert.equal(content, "<div>foo</div><div>babar</div>{{=part.test.undef}}");
+				assert.equal(content, "<div>foo</div>{{=part.undef}}");
 				done();
 			});
 		});
