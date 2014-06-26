2246708ba522c30151ff82d95d0bbbbd121a3c58
Per Stenström <per@vinnovera.se>
2014-06-26 18:38:00 +0200

    Test for nested module

--- test/test.js
+++ test/test.js
@@ -7,12 +7,20 @@ describe('Estrad Template', function() {
 		settings = {
 			"folder": "test/modules"
 		},
-		main = new Buffer("<div>{{=part.main.module}}</div>");
+		main = new Buffer("<div>{{=part.main.module}}</div>"),
+		nested = new Buffer("<div>{{=part.main.module.atom}}</div>");
 
 		describe("Basic", function() {
 			it("should interpolate module.json", function(done) {
 				partials(main, settings, function(err, content) {
 					assert.equal(content, "<div>bar</div>");
+					done();
+				});
+			});
+
+			it("should interpolate template.html", function(done) {
+				partials(nested, settings, function(err, content) {
+					assert.equal(content, "<div>babar</div>");
 					done();
 				});
 			});
