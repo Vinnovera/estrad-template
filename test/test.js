128f878287d9e52d110d3267628447c366557dff
Per Stenström <per@vinnovera.se>
2014-06-27 13:37:54 +0200

    Handle missing partial

--- test/test.js
+++ test/test.js
@@ -8,7 +8,9 @@ describe('Estrad Template', function() {
 			"folder": "test/modules"
 		},
 		main = new Buffer("<div>{=part.main.module}</div>"),
-		nested = new Buffer("<div>{=part.main.module.atom}</div>");
+		nested = new Buffer("<div>{=part.main.module.atom}</div>"),
+		alternative = new Buffer("<div>{=part.main.module.alternative}</div>"),
+		unresolved = new Buffer("<div>{=part.unresolved}</div>");
 
 		describe("Basic", function() {
 			it("should interpolate module.json", function(done) {
@@ -21,6 +23,22 @@ describe('Estrad Template', function() {
 			it("should interpolate template.html", function(done) {
 				partials(nested, settings, function(err, content) {
 					assert.equal(content, "<div>babar</div>");
+					done();
+				});
+			});
+
+			it("should interpolate alternative.json", function(done) {
+				partials(alternative, settings, function(err, content) {
+					assert.equal(content, "<div>foo</div>");
+					done();
+				});
+			});
+		});
+
+		describe("Error handling", function() {
+			it("should keep unresolved partial", function(done) {
+				partials(unresolved, settings, function(err, content) {
+					assert.equal(content, "<div>{=part.unresolved}</div>");
 					done();
 				});
 			});
