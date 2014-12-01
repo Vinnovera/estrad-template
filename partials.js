c4ba208d1310e195be4f1cde8d3c19da1ff0d55d
Per Stenström <per@vinnovera.se>
2014-12-01 13:39:50 +0100

    Break up nested loops

--- partials.js
+++ partials.js
@@ -14,14 +14,14 @@
 			},
 			template,
 			autoload,
-			func;
+			autoloadFunc;
 
 		// If obj is function use obj as callback
 		if(typeof obj === 'function') {
 			callback = obj;
-
-		// Else extend options with obj
 		} else {
+
+			// Else extend options with obj
 			options = extend(options, obj);
 		}
 
@@ -29,21 +29,26 @@
 		autoload = new Autoload(options);
 
 		if(typeof page === 'string') {
+
 			// page is an url
-			func = autoload.loadFile;
-		} else if (typeof page === 'object') {
+			autoloadFunc = autoload.loadFile;
+		} else if(typeof page === 'object') {
+
 			// page is a Buffer object
 			page = page.toString();
-			func = autoload.getPartials;
+			autoloadFunc = autoload.getPartials;
 		} else {
+
+			// Page is not something we can use
 			return callback(new Error());
 		}
 
-		func(page, function(err, result) {
+		autoloadFunc(page, function(err, result) {
 
 			if(err) return callback(err);
+
 			// Solve dependencies
-			template.solveDependencies(result, function(err, page){
+			template.solveDependencies(result, function(err, page) {
 				if(err) return callback(err);
 
 				callback(null, page);
